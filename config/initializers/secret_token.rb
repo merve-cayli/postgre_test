7dcca9961884423d40b84a1a0b884274fa7a1671
¨merve¨ <caylimerve@gmail.com>
2012-09-07 11:17:41 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostgreTest::Application.config.secret_token = '33954dcbe95ce0b2e0fdc6f22e6b563b587637bd72d924ad2a1beb1575e33ac695c784cdbbba6ed5f6061b3e41853a20fd523b2667951d51daac608de3db1d3b'
